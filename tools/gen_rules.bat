9c71efb8f93b770118505d7235a0be2498a2ba89
sunmingbao <sunmingbao@126.com>
2014-02-20 22:09:18 +0800

    增加dos批处理文件版权说明; 修正部分dos批处理文件的错误;

--- tools/gen_rules.bat
+++ tools/gen_rules.bat
@@ -1,6 +1,19 @@
+:: 
+:: 本软件为免费、开源软件。
+:: 本软件的版权(包括源码及二进制发布版本)归一切公众所有。
+:: 您可以自由使用、传播本软件。
+:: 您也可以以任何形式、任何目的使用本软件(包括源码及二进制发布版本)，而不受任何版权限制。
+:: =====================
+:: 作者: 孙明保
+:: 邮箱: sunmingbao@126.com
+::
+
+
+:: 生成makefile规则
+
 @echo off
 set rule_file=c:\rules.txt
-/* 生成makefile规则 */
+
 echo #gen_rules > %rule_file%
 for /f  "delims=  " %%i in (obj\src_2.txt) do (
 echo %%i
--- tools/get_src_files.bat
+++ tools/get_src_files.bat
@@ -1,7 +1,15 @@
+:: 
+:: 本软件为免费、开源软件。
+:: 本软件的版权(包括源码及二进制发布版本)归一切公众所有。
+:: 您可以自由使用、传播本软件。
+:: 您也可以以任何形式、任何目的使用本软件(包括源码及二进制发布版本)，而不受任何版权限制。
+:: =====================
+:: 作者: 孙明保
+:: 邮箱: sunmingbao@126.com
+::
+
 @echo off
 dir src /B > obj\src_1.txt
-
-::tools\str_replace.bat  obj\src_1.txt obj\src_4.txt .c
 
 if not exist obj\src_2.txt goto BEGIN_OUTPUT
 
